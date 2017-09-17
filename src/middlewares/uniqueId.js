07bee71d72731ec3636e6a3ffd9ffb654458208c
astunov <astunov@gmail.com>
2017-09-17 16:30:38 +0300

    Refactor adding unique id to post

--- src/middlewares/uniqueId.js
+++ src/middlewares/uniqueId.js
@@ -1,14 +1,13 @@
 import { FETCH_CITY, FETCH_CURRENT_CITY } from '../actions/types'
-import { v4 } from 'uuid'
 
 export default () => {
   return next => action => {
     let { payload, type } = action
     if (type === FETCH_CITY) {
-      payload = Object.assign(payload, { id: v4() })
+      payload = { ...payload, id: Math.random + Date.now() }
     }
     if (type === FETCH_CURRENT_CITY) {
-      payload = Object.assign(payload, { id: 'currentCity' })
+      payload = { ...payload, id: 'currentCity' }
     }
 
     next(action)
